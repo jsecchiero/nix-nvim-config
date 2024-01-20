27af746472a0958d1c5adbaf0d6bf78f6e28c127
jsecchiero <secchierojacopo@gmail.com>
2024-01-20 15:13:16 +0100

    Switch to nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Neovim opinionated flake";
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs";
+      url = "github:NixOS/nixpkgs/nixos-unstable";
     };
     neovim = {
       url = "github:neovim/neovim/stable?dir=contrib";
