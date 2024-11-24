81619a11fdbe8d98ef2a5f156121155fdeaa446d
jsecchiero <secchierojacopo@gmail.com>
2024-11-24 20:20:21 +0100

    Add telescope jumplist shortcut

--- config/lua/telescope-setup.lua
+++ config/lua/telescope-setup.lua
@@ -60,6 +60,7 @@ vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc =
 vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = 'Search [B]uffers' })
 vim.keymap.set('n', '<leader>g', require('telescope.builtin').live_grep, { desc = 'Search by [G]rep' })
 vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'Search [F]iles' })
+vim.keymap.set('n', '<leader>j', require('telescope.builtin').jumplist, { desc = 'Search [J]umplist' })
 
 vim.keymap.set('n', '<leader>/', function()
     -- You can pass additional configuration to telescope to change theme, layout, etc.
