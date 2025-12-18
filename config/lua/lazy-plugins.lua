a58f484d22f077e2597f61085b94c377f158ef99
jsecchiero <secchierojacopo@gmail.com>
2025-12-18 15:48:44 +0100

    Setup a minimal lualine and show relative path to the buffer file

--- config/lua/lazy-plugins.lua
+++ config/lua/lazy-plugins.lua
@@ -256,6 +256,14 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {},
+        lualine_c = {{'filename', path = 1}},
+        lualine_x = {},
+        lualine_y = {'progress'},
+        lualine_z = {'location'},
+      },
     },
   },
 
